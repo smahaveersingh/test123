da073b8d8c062ce3bae7f83098b442c0a9c7300c
Mahaveer Singh <msingh@obelisk.com>
2021-02-01 14:17:57 +0000

    commit 1

--- screens/ScreenA.js
+++ screens/ScreenA.js
@@ -3,22 +3,8 @@ import { View, StyleSheet, Text, Button } from 'react-native';
 import CalendarPicker from 'react-native-calendar-picker';
 
 const ScreenA = ({ navigation }) => {
-    constructor(props) {
-        super(props);
-        this.state = {
-          selectedStartDate: null,
-        };
-        this.onDateChange = this.onDateChange.bind(this);
-      }
-    
-      onDateChange(date) {
-        this.setState({
-          selectedStartDate: date,
-        });
-      }    
-
-
-    return (
+ 
+   return (
         <View>
             <Text>Screen A</Text>
             <Button title="Navigate to Screen B" onPress={() => navigation.navigate('screenB')}></Button>
